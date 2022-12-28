43a5749810876c1a98a5fd05d6c64ac04edcad0d
rizkihnf <rizkihnf2@gmail.com>
2022-12-28 16:35:54 +0700

    first-payment

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit possimus vero optio recusandae explicabo sed.
+            Omnis enim sed quibusdam delectus reprehenderit! Omnis, voluptas consequuntur! Inventore, odit aliquid quaerat
+            dolorum ab mollitia ratione perferendis, culpa saepe molestias possimus magni animi! Vitae, sequi nam. Voluptas,
+            ab error dignissimos similique cumque architecto! Sit animi quis ipsa vero accusamus excepturi voluptates ipsam
+            hic labore error sapiente maxime, cupiditate quaerat suscipit? Quas, hic a, earum, harum blanditiis amet dicta
+            quaerat quibusdam natus ut odio tempora sunt excepturi alias reiciendis eligendi cum eius. Exercitationem soluta
+            ducimus magnam non earum ut enim sit. Consectetur, ad? Mollitia, tempora?</p>
+    </div>
+@endsection
